1fa0dc97c1f69770c4a4164fbb8e29babf03b7a7
Emmily <e.chelangand11@gmail.com>
2020-12-01 21:25:27 -0600

    second commit

--- AugTDDProject/src/main/java/com/ups/testRunner/UpsTestRunner.java
+++ AugTDDProject/src/main/java/com/ups/testRunner/UpsTestRunner.java
@@ -2,7 +2,9 @@ package com.ups.testRunner;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class UpsTestRunner {
@@ -14,6 +16,29 @@ public class UpsTestRunner {
 	driver.get("https://www.ups.com/lasso/login?");
 	driver.manage().window().maximize();
 	driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+	
+	// identify all the webElement
+	WebElement closeCookiesBunner;
+	WebElement userId;
+	WebElement password;
+	WebElement logInBtn;
+	WebElement errorMsg;
+	
+	WebElement closeCookieBanner = driver.findElement(By.xpath("//*[@id='_tealiumImplicitmodal']/div/button/span[1]"));
+	userId = driver.findElement(By.id("email"));
+	password = driver. findElement(By.name("password"));
+	logInBtn = driver.findElement(By.xpath("//*[@id='submitBtn']"));
+	
+	closeCookieBanner.click();
+	userId.sendKeys("username");
+	password.sendKeys("password");
+	logInBtn.click();
+
+	errorMsg = driver.findElement(By.xpath("//*[@id='errorMessages']"));
+	String msg = errorMsg.getText();
+	return;
+
+	
 	}
 
 }
