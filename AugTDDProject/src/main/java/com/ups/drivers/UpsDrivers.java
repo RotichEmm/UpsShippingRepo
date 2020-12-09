cc0db8771bb43aa8d9f41297a7dde67f03128eb8
Emmily <e.chelangand11@gmail.com>
2020-12-09 10:13:45 -0600

    commit

--- AugTDDProject/src/main/java/com/ups/drivers/UpsDrivers.java
+++ AugTDDProject/src/main/java/com/ups/drivers/UpsDrivers.java
@@ -1,21 +1,25 @@
 package com.ups.drivers;
 
 import org.openqa.selenium.WebDriver;
-
-
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 
 public class UpsDrivers {
 		public WebDriver getChromeDriver() {
 			
-			WebDriverManager.chromedriver().browserVersion("87.0.4280.88").setup();
+			WebDriverManager.chromedriver().browserPath("87.0.4280.88").setup();
+			System.setProperty("webdriver.chrome.driver","/absolute/path/to/binary/chromedriver");
 			WebDriver driver = new ChromeDriver();
 			return driver; 
 		}
 
 		public WebDriver getFirefoxDriver() {
 
-			WebDriverManager.firefoxdriver().browserVersion("83").setup();
+			WebDriverManager.firefoxdriver().browserPath("83").setup();
+			System.setProperty("webdriver.gecko.driver","/absolute/path/to/binary/geckodriver");
 			WebDriver driver = new FirefoxDriver();
 			return driver;
 		}
