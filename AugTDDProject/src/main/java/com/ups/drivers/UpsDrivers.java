45ea0260de4e48fadde90798dfed44a05e125e15
Emmily <e.chelangand11@gmail.com>
2020-12-08 19:24:17 -0600

    commit

--- AugTDDProject/src/main/java/com/ups/drivers/UpsDrivers.java
+++ AugTDDProject/src/main/java/com/ups/drivers/UpsDrivers.java
@@ -2,9 +2,7 @@ package com.ups.drivers;
 
 import org.openqa.selenium.WebDriver;
 
-
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class UpsDrivers {
